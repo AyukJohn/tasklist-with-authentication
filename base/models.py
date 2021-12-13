86b5e3d7ad5c25f78785b621c74d1d573970a16a
johnayuk <john1234ayuk@gmail.com>
2021-12-13 15:44:33 +0100

    tasklist commit

---
+++ base/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    complete = models.BooleanField(default=False)
+    created = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, null=True,on_delete= models.CASCADE, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        ordering=['complete']
