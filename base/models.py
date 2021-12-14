89f55bfe2e3072c61f61b2878a61d520701c86f7
johnayuk <john1234ayuk@gmail.com>
2021-12-14 12:41:28 +0100

    made changes to the model field of description from charfild to textfield

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
     complete = models.BooleanField(default=False)
     created = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, null=True,on_delete= models.CASCADE, blank=True)
